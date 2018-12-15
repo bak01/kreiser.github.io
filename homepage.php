14abc8c753ec65b94acab6550b3daa06a2925ed9
bak01 <bkreiser5@gmail.com>
2018-12-14 21:51:59 -0500

    Create homepage.php

---
+++ homepage.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Bak Homepage</title>
+    </head>
+    <body style ="background-color:black;text-align:center;">
+        <img src="helloworld.jpg" alt="helloworld.jpg" width="500" height="220">
+        <p style="color:#41FF00; font-family:courier; font-size:250%"><b>Hello Brandon, Welcome to your homepage!</b></p>
+
+        <pre style= "color:#41FF00; font-family:courier; font-size:160%;text-align:left;"><b>     School                                               Regular                                      Stores</pre>
+        <a href = "https://www.google.com">Google</a>
+        <a href = "https://www.youtube.com">Youtube</a>
+        <a href = "https://mail.google.com/mail/ca/u/0/?shva=1#inbox">Email</a>
+
+    </body>
+</html>
